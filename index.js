73ad7adf5e10521d0773cbeea5542345a7a5065c
Patrick Kipkoech <patrickwayy@gmail.com>
2024-09-18 15:46:02 +0300

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name : "Patrick",
+    streetAddress:"Mirema 1234"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return { ...employee,[key]:value }; 
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmploy = { ...employee};
+    delete newEmploy[key];
+    return newEmploy;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
+
+
+
